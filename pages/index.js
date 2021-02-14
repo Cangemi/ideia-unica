c40b62d1fa82863697bce44a480f928e3c30fce9
Cangemi <65517004+Cangemi@users.noreply.github.com>
2021-02-14 15:22:18 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,7 @@
-  
-import { useState } from 'react';
-
 function Home(){
-    return (
-        <div>
-            <h1>Home</h1>
-            <Contador/>
-            <h3>Teste</h3>
-        </div>
-    )
-}
-
-function Contador(){
-
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador (contador + 1);
-    }
     return(
         <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador} >Adicionar</button>
+            <h1>Home</h1>
         </div>
     )
 }
