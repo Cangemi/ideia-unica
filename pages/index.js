fdfb22b8c18e6fa845238ea3cd8a5e810372c5e9
Cangemi <65517004+Cangemi@users.noreply.github.com>
2021-02-15 01:30:09 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Menu from '../components/Menu';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -5,6 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Home(){
     return(
         <div>
+            <Head>
+                <title>Home - Cangemi</title>
+                <meta name='description' content='Site de estudo'/>
+                <meta name='author' content='Pedro Cangemi'/>
+            </Head>
             <Menu />
             Home
             
